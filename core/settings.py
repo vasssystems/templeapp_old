bb628da3e9774dfe0cfb74bdc06ead31d938f4b8
Anand VM <anandnambissan2@gmail.com>
2023-12-26 16:56:49 +0530

    Updated cors middileware

--- core/settings.py
+++ core/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -321,3 +322,5 @@ CORS_ALLOW_HEADERS = [
     "x-csrftoken",
     "x-requested-with",
 ]
+
+APPEND_SLASH = True
